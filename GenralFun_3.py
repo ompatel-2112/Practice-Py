145e9bd79801594f1d005e8aa348feefab6a18eb
ompatel-2112 <ompatel43456@gmail.com>
2026-07-30 16:29:14 +0530

    Add files via upload

---
+++ GenralFun_3.py
@@ -0,0 +1,32 @@
+"""
+Genral function
+
+sum()
+len()
+max()
+min()
+"""
+
+a="hello" #5
+
+b="world"
+
+print(a)
+
+print(len(a))
+
+x=5
+y=3
+
+h=[5,3]
+
+print(type(h))
+
+total =sum(h)
+print(total)
+
+print(x+y)
+
+print(min(a))
+
+print(max(a))
