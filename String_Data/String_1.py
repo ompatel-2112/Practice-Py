10f559f12c98aedcca3f63b841f9a4ec886adee5
ompatel-2112 <ompatel43456@gmail.com>
2026-08-04 15:48:09 +0530

    Add files via upload

---
+++ String_Data/String_1.py
@@ -0,0 +1,24 @@
+s = "hellow world" # this is example of string where s is variable and "hello world" is string data type
+
+print(s)
+
+print(s[0])
+
+print(s[1:5])
+
+print(s[3:])
+
+print(s[::2]) # print each 2nd elements from string
+
+print(s*2)
+
+print(s + "hello" )
+
+print(s+'1')
+
+#print(s + 1) this shows error bcs we only merge same type of data type
+
+print(s,'hello') # for create space use ","
+
+print(s,12)
+
