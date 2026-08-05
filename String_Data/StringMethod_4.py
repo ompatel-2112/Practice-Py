e421fae61bf69a4a43b2e571a230ffdb6c18c2e7
ompatel-2112 <ompatel43456@gmail.com>
2026-08-05 16:01:01 +0530

    Update StringMethod_4.py

--- String_Data/StringMethod_4.py
+++ String_Data/StringMethod_4.py
@@ -3,31 +3,31 @@ a="Hello world"
 
 b="new"
 
-print(a.capitalize())
+print(a.capitalize()) # Capitalize First Char
 
-print(a.isdigit())
+print(a.isdigit())  # Chack If String Is Digit Than Return True
 
-print(a.isupper())
+print(b.isupper())  # Check If String Starting is Upper Than Return string else False
 
-print(a.lower())
+print(a.lower())    # Check If String Starting is Lower Than Return string else False
 
-print(a.find("h"))
+print(a.find("h"))  # Check if Var Present in string Than returen Index
 
-print(a.replace("H","p"))
+print(a.replace("H","p"))  # Replace Right Parameter in String
 
-print(a.split(" "))
+print(a.split(" "))  # split the string from space or given Parameter
 
 c="*"
 
-print(c.join(a))
+print(c.join(a))     # join the another object from split or space
 
-print(a.count("l"))
+print(a.count("l"))  # Count How many time Appear 
 
-print(a.upper())
+print(a.upper())     # Convert string in to upper
 
-print(a.lower())
+print(a.lower())     # Convert String in to lower
 
 w="...how are you..."
 
-print(w.strip("."))
+print(w.strip("."))  # Remove unwanted special Char or given Parameter
 
